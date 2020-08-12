f679339bda812ddedd56f8c7e295f354cc66f465
Arnold Pfahnl <ajpfahnl@gmail.com>
2020-08-12 12:01:46 -0600

    add cell proportion check

--- save.R
+++ save.R
@@ -1,3 +1,9 @@
+# save
 saveRDS(ret.list, file = "ret-list.rds")
 saveRDS(ret.anchors, file = "ret-anchors.rds")
 saveRDS(ret.combined, file = "ret-combined.rds")
+
+# load
+ret.list <- readRDS("ret-list.rds")
+ret.anchors <- readRDS("ret-anchors.rds")
+ret.combined <- readRDS("ret-combined.rds")
