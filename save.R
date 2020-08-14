05a0f8848611e97f05991180065137ba7afefea9
Arnold Pfahnl <ajpfahnl@gmail.com>
2020-08-14 02:07:10 -0600

    add gene enrichment analysis

--- save.R
+++ save.R
@@ -2,8 +2,10 @@
 saveRDS(ret.list, file = "ret-list.rds")
 saveRDS(ret.anchors, file = "ret-anchors.rds")
 saveRDS(ret.combined, file = "ret-combined.rds")
+saveRDS(cells.diffgenes, file = "cells-diffgenes.rds")
 
 # load
 ret.list <- readRDS("ret-list.rds")
 ret.anchors <- readRDS("ret-anchors.rds")
 ret.combined <- readRDS("ret-combined.rds")
+cells.diffgenes <- readRDS("cells-diffgenes.rds")
